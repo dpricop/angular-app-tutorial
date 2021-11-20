033cedbd22cd54bfcfacd22cb45c488d7f5cdd55
dpricop <pricopdumitru94@gmail.com>
2021-11-20 22:36:02 +0200

    application routing has been added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TestCmpComponent } from "./test-cmp/test-cmp.component";
+
+const routes: Routes = [
+  { path: 'test', component: TestCmpComponent },
+  { path: 'test-component', component: TestCmpComponent },
+  { path: '', redirectTo: 'test', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
+import { MainNavComponent } from './main-nav/main-nav.component';
+
 import { MaterialModule } from "./material/material.module";
-import { TestCmpComponent } from './test-cmp/test-cmp.component';
+import { AppRoutingModule } from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainNavComponent,
-    TestCmpComponent
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     LayoutModule,
-    MaterialModule
+    MaterialModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
