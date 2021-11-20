9e9ceddcdfb7708815b3888bd1959c5988b1acbf
dpricop <pricopdumitru94@gmail.com>
2021-11-20 22:17:09 +0200

    components (main-nav, test-cmp) has been added, all material components has been included in material.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MaterialModule } from "./material/material.module";
+import { TestCmpComponent } from './test-cmp/test-cmp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    TestCmpComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
